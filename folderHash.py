a9970500b08b98de5c34fec9a58d9ee5d78eb55f
Strangelovian <strangelovian+github@gmail.com>
2015-08-24 23:03:54 +0200

    fix issue with csv file end of line character on windows change to less computational intensive hash for usage on less powerful processors generate file name including the current local date time

--- folderHash.py
+++ folderHash.py
@@ -4,6 +4,7 @@ import os
 import csv
 import hashlib
 import base64
+import datetime
 
 def strippath(base, path, filename):
     stripped = os.path.join(path, filename)
@@ -13,20 +14,25 @@ def strippath(base, path, filename):
     return stripped
 
 def base64hash(pathtofile):
-    hasher = hashlib.sha256()
+    hasher = hashlib.md5()
     with open(pathtofile, 'rb') as filetohash:
         while True:
-            data=filetohash.read(hasher.block_size)
+            data=filetohash.read(3*1024*1024)
             if not data: break
             hasher.update(data)
     return base64.b64encode(hasher.digest()).decode("utf-8")
 
+#t1=datetime.datetime.utcnow()
+
 base = sys.argv[1]
 (drive, basepath) = os.path.splitdrive(base)
-outputfilename = os.path.realpath(os.path.normpath(basepath)).replace(os.path.sep, '_') + '.csv'
-
+outputfilename = datetime.datetime.now().strftime("%Y%m%d.%H%M%S.") + os.path.normpath(basepath).replace(os.path.sep, '_') + '.csv'
+print(outputfilename)
 with open(outputfilename, 'w+') as outputfile:
-    outputcsv=csv.writer(outputfile)
+    outputcsv=csv.writer(outputfile, lineterminator="\n")
     for path, dirnames, filenames in os.walk(base):
         for filename in fnmatch.filter(filenames, '*.*'):
             outputcsv.writerow([strippath(base, path, filename), base64hash(os.path.join(path, filename))])
+
+#t2=datetime.datetime.utcnow()
+#print(t2-t1)
