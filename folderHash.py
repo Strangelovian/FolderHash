bfe4853f072685dbffa937ff26e7169f428c1e9e
Strangelovian <strangelovian+github@gmail.com>
2016-05-05 19:48:25 +0200

    change file format to facilitate audit file comparison

--- folderHash.py
+++ folderHash.py
@@ -1,19 +1,17 @@
 import sys
 import fnmatch
 import os
-import csv
 import hashlib
-import base64
 import datetime
 
-def strippath(base, path, filename):
+def strip_path(base, path, filename):
     stripped = os.path.join(path, filename)
     stripped = os.path.relpath(stripped, start=base)
     stripped = os.path.normpath(stripped)
     stripped = stripped.replace(os.path.sep, '/')
     return stripped
 
-def md5hash(pathtofile):
+def md5_hash(pathtofile):
     hasher = hashlib.md5()
     with open(pathtofile, 'rb') as filetohash:
         while True:
@@ -25,16 +23,19 @@ def md5hash(pathtofile):
 base = sys.argv[1]
 (drive, basepath) = os.path.splitdrive(base)
 outputfilename = os.path.normpath(basepath).replace(os.path.sep, '_') + '.txt'
-with open(outputfilename, 'w+') as outputfile:
+
+with open(outputfilename, mode='w', encoding='utf8', newline='\n') as outputfile:
     begin_time = datetime.datetime.now()
     outputfile.write(begin_time.strftime("%Y%m%d%H%M%S\n"))
     for path, dirnames, filenames in os.walk(base):
         dirnames.sort()
+        filenames.sort()
         for filename in fnmatch.filter(filenames, '*.*'):
-            outputfile.write(md5hash(os.path.join(path, filename)) + ' ' + strippath(base, path, filename) + '\n')
+            outputfile.write(strip_path(base, path, filename) + ' ' + md5_hash(os.path.join(path, filename)) + '\n')
     end_time = datetime.datetime.now()
     outputfile.write(end_time.strftime("%Y%m%d%H%M%S\n"))
     processing_time = end_time - begin_time
     outputfile.write(str(processing_time))
-with open(outputfilename + '.md5', 'w+') as md5checksumfile:
-    md5checksumfile.write(md5hash(outputfilename) + '\n')
+
+with open(outputfilename + '.md5', mode='w', encoding='utf8', newline='\n') as md5checksumfile:
+    md5checksumfile.write(md5_hash(outputfilename) + '\n')
